9c29415587e167b481dfac58df923ea1ce24b480
rraven5571 <romkabasalai@gmail.com>
2024-01-22 20:32:57 +0300

    First_commit

---
+++ Source/MyProject4/MyProject4GameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MyProject4GameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMyProject4GameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMyProject4GameMode();
+};
+
+
+
